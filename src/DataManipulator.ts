689dd57fa84ef4714de090e61f9298e75bc9d4db
Sooraj <76109789+thesoorajvarrma@users.noreply.github.com>
2022-10-07 01:42:20 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,19 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price abc: number,
+  price def: number,
+  ratio: number,
   timestamp: Date,
+  upper bound: number,
+  lower bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+    const priceABC = (serverRespond[0].top ask.price + server Respond[@].top bid.price) / 2;
+    const priceDEF = (serverRespond[1].top ask.price + server Respond[1].topbid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    return {
+      price abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: server Respond[0].timestamp > server Respond[1].timestamp?
+        server Respond[@].timestamp : server respond[1].timestamp, 
+        upper_bound: upperBound, 
+        lower_bound: lowerBound, 
+        trigger alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
       };
     })
   }
